0b3e1724cdcff1b319bac678f2c02b24cec8e9af
Yusuke-i <yinadaroma@gmail.com>
2026-03-15 00:05:44 +0900

    cording01 complete

--- js/script.js
+++ js/script.js
@@ -5,3 +5,21 @@ $(function () {
     $(".js-drawer").fadeToggle();
   });
 });
+
+$(function () {
+  // ヘッダーの高さ分だけコンテンツを下げる
+  const height = $(".js-header").height();
+  $("main").css("margin-top", height);
+
+  // ヘッダーの高さ取得
+  const headerHeight = $(".js-header").height();
+  $('a[href^="#"]').click(function () {
+    const speed = 600;
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? "html" : href);
+    // ヘッダーの高さ分下げる
+    let position = target.offset().top - headerHeight;
+    $("body,html").animate({ scrollTop: position }, speed, "swing");
+    return false;
+  });
+});
